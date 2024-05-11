64bd2c04b98e968c6c6eb7c28294e62d6cc4ebcd
Priyanshu Omer <146474281+Priyanshuomer@users.noreply.github.com>
2024-05-11 21:10:49 +0530

    Create Rotate_array.cpp

---
+++ Rotate_array.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int n,i,j,k;
+    cin>>n;
+    vector<int> a(n);
+    vector<int> temp(n);
+    for(i=0;i<n;i++)
+    {
+    cin>>a[i];
+    }
+    cin>>k;
+    k%=n;
+    for(i=n-k,j=0;i<n;i++)
+    {
+    temp[j++]=a[i];
+    }
+    for(i=0;i<n-k;i++)
+    {
+    temp[j++]=a[i];
+    }
+    for(i=0;i<n;i++)
+    a[i]=temp[i]; 
+    for(i=0;i<n;i++)
+    {
+    cout<<a[i]<<" ";
+    }
+    return 0;
+}
